7e925fa23035eed0481b97843e844abe7293d3dc
Sarah Ganz <strolisarah@gmail.com>
2023-08-27 20:14:29 -0400

    css on achievedWishesPage

--- src/pages/AchievedWishesPage/AchievedWishesPage.jsx
+++ src/pages/AchievedWishesPage/AchievedWishesPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import * as usersAPI from "../../utilities/users-api";
+import "./AchievedWishesPage.css"; // Import your custom CSS for additional styling
 
 export default function AchievedWishesPage() {
   const [achievedWishes, setAchievedWishes] = useState([]);
@@ -20,13 +21,21 @@ export default function AchievedWishesPage() {
   };
 
   return (
-    <div>
-      <h2>Achieved Wishes</h2>
+    <div className="achieved-wishes-container">
+      <h2>Your Achieved Wishes</h2>
+      <h2 className="achieved-wishes-header">
+        <span className="achieved-wishes-header-item country-column">
+          Country
+        </span>
+        <span className="achieved-wishes-header-item state-column">State</span>
+      </h2>
       <ul>
         {achievedWishes.map((item) => (
-          <li key={item._id}>
-            <Link to={`/achieved-wishes/${item._id}`}>
-              Country: {item.country}, State: {item.state}
+          <li key={item._id} className="achieved-wishes-item">
+            <span className="country-column">{item.country}</span>
+            <span className="state-column">{item.state}</span>
+            <Link to={`/achieved-wishes/${item._id}`} className="link-button">
+              View Details
             </Link>
           </li>
         ))}
